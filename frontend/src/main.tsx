06b4d3f28c9627d1c9752aca83aa09cdc62eb3ac
mrcloud1 <lolord1224@gmail.com>
2026-01-28 09:41:00 +0100

    chore: add posthog tracking

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,12 +1,29 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'  
+import React from 'react'
+import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
-import { Toaster } from 'sonner'
+import './index.css'
+import posthog from 'posthog-js'
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
+// Importem les variables d'entorn
+const posthogKey = import.meta.env.VITE_POSTHOG_KEY;
+const posthogHost = import.meta.env.VITE_POSTHOG_HOST;
+
+// Comprovem que existeixen abans d'inicialitzar (bona pràctica)
+if (posthogKey && posthogHost) {
+  posthog.init(posthogKey, {
+      api_host: posthogHost,
+      person_profiles: 'identified_only', 
+      session_recording: {
+          maskAllInputs: false,
+          maskInputOptions: {
+              password: true
+          }
+      }
+  })
+}
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
     <App />
-    <Toaster position="top-center" richColors closeButton />
-  </StrictMode>,
+  </React.StrictMode>,
 )
